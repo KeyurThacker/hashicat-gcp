1c53099d9da49c147103f4852bbfde098a2c8b92
Keyur Thacker <keyurthacker01@gmail.com>
2021-11-05 13:28:37 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,14 @@
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+module "network" {
+  source       = "app.terraform.io/hashicat-keyur/network/google"
+  version      = "3.4.0"
+  network_name = "gaurav-network"
+  project_id   = var.project
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+  # insert required variables here
+}
